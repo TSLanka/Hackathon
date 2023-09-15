e306a36802256dace94ee911632744473aad3005
Teja Naga Subhash Lanka <103932692+TSLanka@users.noreply.github.com>
2023-09-15 10:20:39 +0530

    .....

--- modified_bert_cluster_unlabeled_data.py
+++ modified_bert_cluster_unlabeled_data.py
@@ -8,6 +8,25 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans  # Import K-Means clustering
 from sklearn.svm import LinearSVC
 
+import torch
+from transformers import BertTokenizer, BertModel
+
+# Load pre-trained BERT model and tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertModel.from_pretrained('bert-base-uncased')
+
+def get_bert_embeddings(texts):
+    # Convert texts to BERT embeddings
+    embeddings = []
+    for text in texts:
+        inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True, max_length=512)
+        with torch.no_grad():
+            outputs = model(**inputs)
+        embeddings.append(outputs.last_hidden_state.mean(dim=1).squeeze().numpy())
+    return embeddings
+
+
+
 # Load spaCy English Model and NLTK Stopwords
 nlp = spacy.load("en_core_web_sm")
 nltk.download('stopwords', quiet=True)
@@ -29,7 +48,7 @@ def preprocess_data(data):
     return data
 
 # Load the labeled data (merged with K-Means pseudo-labels) and preprocess it
-labeled_data_path = r"C:\Users\TSLanka\Documents\GitHub\Hackathon\projectdescriptions.csv"
+labeled_data_path = r"C:\Users\TSLanka\Documents\GitHub\Hackathon\labeled_data.csv"
 labeled_data = pd.read_csv(labeled_data_path)
 
 # Add the Cluster column from kmeans_clusters_output.csv
@@ -38,7 +57,27 @@ labeled_data['Cluster'] = kmeans_clusters_data['Cluster']
 
 labeled_data = preprocess_data(labeled_data)
 
-# Apply TF-IDF Vectorization on labeled data
+
+# Get BERT embeddings for labeled data
+X_labeled_embeddings = get_bert_embeddings(labeled_data['cleaned_text'].tolist())
+
+# Create a K-Means clustering model with explicit n_init value
+kmeans = KMeans(n_clusters=5, random_state=0, n_init=10)
+kmeans.fit(X_labeled_embeddings)
+
+
+# Load and preprocess the unlabeled data
+unlabeled_data_path = r"C:/Users/TSLanka/Documents/GitHub/Hackathon/merged_data.csv"
+unlabeled_data = pd.read_csv(unlabeled_data_path)
+unlabeled_data = preprocess_data(unlabeled_data)
+
+
+# Get BERT embeddings for unlabeled data
+X_unlabeled_embeddings = get_bert_embeddings(unlabeled_data['cleaned_text'].tolist())
+
+# Cluster the unlabeled data using K-Means
+cluster_labels = kmeans.predict(X_unlabeled_embeddings)
+
 vectorizer = TfidfVectorizer()  
 X_labeled = vectorizer.fit_transform(labeled_data['cleaned_text'])
 y_labeled = labeled_data['Cluster']
@@ -47,9 +86,6 @@ y_labeled = labeled_data['Cluster']
 kmeans = KMeans(n_clusters=10, random_state=0, n_init=20)  # Change the number of clusters and n_init value here
 kmeans.fit(X_labeled)
 
-# Train the SVM on labeled data, explicitly setting dual to 'auto'
-clf = LinearSVC(random_state=0, max_iter=10000, dual='auto')
-clf.fit(X_labeled, y_labeled)
 
 # Load and preprocess the unlabeled data
 unlabeled_data_path = r"C:/Users/TSLanka/Documents/GitHub/Hackathon/merged_data.csv"
@@ -59,9 +95,10 @@ unlabeled_data = preprocess_data(unlabeled_data)
 # Transform the unlabeled data using the vectorizer fitted on labeled data
 X_unlabeled = vectorizer.transform(unlabeled_data['cleaned_text'])
 
-# Predict labels for the unlabeled data
-predicted_labels = clf.predict(X_unlabeled)
-unlabeled_data['Predicted_Label'] = predicted_labels
+# Cluster the unlabeled data using K-Means
+cluster_labels = kmeans.predict(X_unlabeled)
+unlabeled_data['Cluster_Label'] = cluster_labels
 
-# Save the labeled data
-unlabeled_data.to_csv('labeled_data.csv', index=False)
+# Save the clustered data
+unlabeled_data.to_csv('C:/Users/TSLanka/Documents/GitHub/Hackathon/clustered_data.csv', index=False)
+
